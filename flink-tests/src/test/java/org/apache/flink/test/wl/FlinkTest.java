44267510168d8f507008d868062997e5e6f09779
WinderWL <33485062@qq.com>
2020-11-21 13:17:36 +0800

    test
    Signed-off-by: WinderWL <33485062@qq.com>

---
+++ flink-tests/src/test/java/org/apache/flink/test/wl/FlinkTest.java
@@ -0,0 +1,13 @@
+package org.apache.flink.test.wl;
+
+/**
+ * Description
+ *
+ * @author Allen
+ * @date 2020/11/21
+ */
+public class FlinkTest {
+	public static void main(String[] args) {
+	System.out.println("test");
+	}
+}
